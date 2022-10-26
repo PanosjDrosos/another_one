fcafa246ddf3131ce574e31ddc4873716a7758b3
PanosjDrosos <panagiotis.drosos93@gmail.com>
2022-10-26 23:42:59 +0300

    stage 3

--- GOW/Source/name panel.py
+++ GOW/Source/name panel.py
@@ -1,7 +1,7 @@
 import sys
 from typing import Container
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QComboBox, QStackedLayout, QHBoxLayout
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,34 +13,47 @@ class MainWindow(QMainWindow):
         self.username.setText("state your name")
         self.color=QLabel()
         self.color.setText("pick a color")
+        self.stacklayout = QStackedLayout()
+        self.combobox=QComboBox()
+        self.combobox.addItems(["red","green","blue","yellow"])
+        
 
 
 
 
         self.input = QLineEdit()
         #self.input.textChanged.connect()
-        layout= QVBoxLayout()
+        self.layout= QVBoxLayout()
 
 
-        button=QPushButton("press me!")
+        button=QPushButton("start game")
         button.setCheckable(True)
         button.clicked.connect(self.welcome_warrior)
+        self.layout.addWidget(self.username)
+        self.layout.addWidget(self.input)
+
         
-        layout.addWidget(self.username)
-        layout.addWidget(self.input)
-        layout.addWidget(self.color)
-        layout.addWidget(button)
+        self.layout.addWidget(self.color)
+        self.layout.addWidget(self.combobox) 
+        self.layout.addWidget(button)
 
         container=QWidget()
-        container.setLayout(layout)
+        container.setLayout(self.layout)
     
 
         self.setCentralWidget(container)
         self.setFixedSize(QSize(400, 300))
 
     def welcome_warrior(self):
-        print("Get ready to battle")
+        player_name=self.input.text()
+        print(player_name)
+        if player_name=="":
+            print("you forgot to put username")
+        else:
+            print("welcome to the battlefield")
+        colour=self.combobox.currentText()
 
+        print(colour)
 
 app=QApplication(sys.argv)
 window=MainWindow()
